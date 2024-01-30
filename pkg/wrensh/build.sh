23f2e891357b158112ec3913251f9aceefde40cf
Ryan Sepassi <rsepassi@gmail.com>
2024-01-29 17:37:50 -0800

    use OPT

--- pkg/wrensh/build.sh
+++ pkg/wrensh/build.sh
@@ -6,7 +6,7 @@ ARCH=${ARCH:-native}
 OPT=${OPT:-s}
 
 mkdir -p $BUILD_OUT/bin
-zig cc --target=$ARCH -s -static -o $BUILD_OUT/bin/wrensh \
+zig cc -O$OPT --target=$ARCH -s -static -o $BUILD_OUT/bin/wrensh \
   -I $BUILD_DEPS/wren/include \
   $BUILD_PKG/wrensh.c \
   $BUILD_DEPS/wren/lib/libwren.a
