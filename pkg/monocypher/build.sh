75cd6b0e654ebb738e36ee71005a4d792049aaee
Ryan Sepassi <rsepassi@gmail.com>
2024-02-01 17:21:53 -0800

    add monocypher and mbedtls

---
+++ pkg/monocypher/build.sh
@@ -0,0 +1,17 @@
+set -e
+
+file=monocypher.tar.gz
+fetch_urltxt $BUILD_PKG/url.txt $file
+src=$(mktemp -d)
+untar $BUILD_DEPS/$file $src
+
+cd $src/src
+
+cc --target=$ARCH -O$OPT -c monocypher.c -o monocypher.o
+cc --target=$ARCH -O$OPT -c optional/monocypher-ed25519.c -o monocypher-ed25519.o -I.
+ar rcs libmonocypher.a monocypher.o monocypher-ed25519.o
+
+mkdir -p $BUILD_OUT/lib $BUILD_OUT/include
+cp libmonocypher.a $BUILD_OUT/lib
+cp monocypher.h $BUILD_OUT/include
+cp optional/monocypher-ed25519.h $BUILD_OUT/include
