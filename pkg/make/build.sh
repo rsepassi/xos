1b4a040212ef24d210b1c881454322ae616918da
Ryan Sepassi <rsepassi@gmail.com>
2024-02-06 10:58:15 -0800

    add make

---
+++ pkg/make/build.sh
@@ -0,0 +1,12 @@
+fetch_urltxt $BUILD_PKG/url.txt make.tar.gz
+src=$(mktemp -d)
+untar $BUILD_DEPS/make.tar.gz $src
+cd $src
+cp $BUILD_PKG/build.zig .
+cp $BUILD_PKG/arch/config-$ARCH_OS-$ARCH_ISA.h src/config.h
+cp $BUILD_PKG/genheaders/alloca.h lib/
+cp $BUILD_PKG/genheaders/fnmatch.h lib/
+cp $BUILD_PKG/genheaders/glob.h lib/
+printf '%s\n%s\n' "#include <makeint.h>" "$(cat lib/concat-filename.c)" > lib/concat-filename.c
+zig build -Dtarget=$ARCH -Doptimize=$OPT_ZIG
+ln -s $PWD/zig-out/bin $BUILD_OUT
