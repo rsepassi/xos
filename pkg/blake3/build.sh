dadcf86aafec9a331cd528dcf22a8c5e1593ce14
Ryan Sepassi <rsepassi@gmail.com>
2024-02-06 19:36:10 -0800

    add blake3

---
+++ pkg/blake3/build.sh
@@ -0,0 +1,13 @@
+file=blake3.tar.gz
+fetch \
+  "https://github.com/BLAKE3-team/BLAKE3/archive/refs/tags/1.5.0.tar.gz" \
+  $file \
+  "f506140bc3af41d3432a4ce18b3b83b08eaa240e94ef161eb72b2e57cdc94c69"
+src=$(mktemp -d)
+untar $BUILD_DEPS/$file $src
+cd $src
+
+cp $BUILD_PKG/build.zig .
+zig build -Dtarget=$ARCH -Doptimize=$OPT_ZIG
+ln -s $PWD/zig-out/include $BUILD_OUT
+ln -s $PWD/zig-out/lib $BUILD_OUT
