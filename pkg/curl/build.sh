5b4f16c8245bc4b5a7ea9a07cfaeb52335cf8ef9
Ryan Sepassi <rsepassi@gmail.com>
2024-02-13 07:55:21 -0800

    enable more curl features, add brotli

--- pkg/curl/build.sh
+++ pkg/curl/build.sh
@@ -7,19 +7,27 @@ src=$(mktemp -d)
 untar "$BUILD_DEPS/$file" "$src"
 cd $src
 
-cp "$BUILD_PKG/build.zig" .
-cp "$BUILD_PKG/arch/config-$ARCH_OS.h" lib/curl_config.h
-
 if [ "$ARCH_OS" = "macos" ]
 then
   need macossdk
   zigopt="-Dsysroot=$BUILD_DEPS/macossdk"
 fi
 
-zig build -Doptimize=$OPT_ZIG -Dtarget=$ARCH $zigopt -Dmbedtls="$BUILD_DEPS/mbedtls"
+# build
+cp "$BUILD_PKG/build.zig" .
+cp "$BUILD_PKG/arch/config-$ARCH_OS.h" lib/curl_config.h
+zig build -Doptimize=$OPT_ZIG -Dtarget=$ARCH $zigopt \
+  -Dmbedtls="$BUILD_DEPS/mbedtls" \
+  -Dbrotli="$BUILD_DEPS/brotli"
+
+# fetch certificate bundle
+fetch "https://curl.se/ca/cacert-2023-12-12.pem" \
+  cacert.pem \
+  "ccbdfc2fe1a0d7bbbb9cc15710271acf1bb1afe4c8f1725fe95c4c7733fcbe5a"
 
 # install
 ln -s $PWD/zig-out/bin "$BUILD_OUT/bin"
 ln -s $PWD/zig-out/lib "$BUILD_OUT/lib"
-mkdir -p "$BUILD_OUT/include/curl"
+mkdir -p "$BUILD_OUT/include/curl" "$BUILD_OUT/share"
 cp "$src/include/curl"/*.h "$BUILD_OUT/include/curl"
+cp "$BUILD_DEPS/cacert.pem" "$BUILD_OUT/share"
