5cedbd525d4fe00943188e3366c40fe57bce7589
Ryan Sepassi <rsepassi@gmail.com>
2024-02-19 19:18:48 -0800

    wrensh execv added

--- pkg/wrensh/build.sh
+++ pkg/wrensh/build.sh
@@ -1,5 +1,9 @@
 mkdir -p $BUILD_OUT/bin
-cc -O$OPT --target=$ARCH -s -static -o $BUILD_OUT/bin/$(zigi exe wrensh) \
+
+zig build-exe -target $ARCH -O $OPT_ZIG \
+  "$BUILD_PKG/wrensh.c" \
   -I $BUILD_DEPS/wren/include \
-  $BUILD_PKG/wrensh.c \
-  $BUILD_DEPS/wren/lib/$(zigi lib wren) -lc
+  "$BUILD_DEPS"/wren/lib/$(zigi lib wren) \
+  -lc
+
+mv $(zigi exe wrensh) "$BUILD_OUT/bin"
--- pkg/wrensh/wrensh.c
+++ pkg/wrensh/wrensh.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <string.h>
+#include <unistd.h>
 
 #include "wren.h"
 
@@ -19,6 +20,8 @@ typedef struct {
   int argc;
   char** argv;
 } Ctx;
+
+static const int maxpathlen = 1024;
 
 void wrenWriteFn(WrenVM* vm, const char* text) {
   fprintf(stderr, "%s", text);
@@ -61,6 +64,7 @@ void wrenRead(WrenVM* vm) {
   }
   buf[i] = '\0';
   wrenSetSlotBytes(vm, 0, buf, i);
+  free(buf);
 }
 
 void wrenReadN(WrenVM* vm) {
@@ -71,6 +75,7 @@ void wrenReadN(WrenVM* vm) {
   CHECK(buf != NULL, "could not allocate %d bytes for stdin", n);
   int nread = fread(buf, 1, n, stdin);
   wrenSetSlotBytes(vm, 0, buf, nread);
+  free(buf);
 }
 
 void wrenWrite(WrenVM* vm) {
@@ -106,6 +111,74 @@ void wrenExit(WrenVM* vm) {
   exit(n);
 }
 
+void which(char* exe, char* path) {
+  int exelen = strlen(exe);
+  if (exe[0] == '/' || exe[0] == '\\') {
+    // absolute path
+    memcpy(path, exe, exelen);
+    return;
+  }
+
+  char cmd[128] = "which ";
+
+  int cmdlen = 6 + exelen;
+  memcpy(cmd + 6, exe, exelen);
+  cmd[cmdlen] = '\0';
+
+  FILE* fp = popen(cmd, "r");
+  CHECK(fp != NULL, "which failed to spawn");
+
+  char* out = fgets(path, maxpathlen, fp);
+  CHECK(out != NULL, "which stdout read failed");
+  int outlen = strlen(out);
+  CHECK(out[outlen - 1] == '\n', "path is too long");
+  out[outlen - 1] = '\0';
+
+  int returnCode = pclose(fp);
+  CHECK(returnCode == 0, "command %s not found", exe);
+}
+
+void wrenExec(WrenVM* vm) {
+  WrenType t = wrenGetSlotType(vm, 1);
+  CHECK(t == WREN_TYPE_LIST, "must pass a list to execv");
+  int nslots = wrenGetSlotCount(vm);
+
+  wrenEnsureSlots(vm, 4);
+  int argc = wrenGetListCount(vm, 1);
+  const char** argv = malloc((argc + 1) * sizeof(char*));
+  CHECK(argv != NULL, "malloc failed");
+  argv[argc] = NULL;
+  for (int i = 0; i < argc; ++i) {
+    wrenGetListElement(vm, 1, i, 3);
+    const char* warg = wrenGetSlotString(vm, 3);
+    argv[i] = warg;
+  }
+
+  // path lookup using "which"
+  char path[maxpathlen];
+  which(argv[0], &path);
+
+  if (nslots == 3) {
+    // with env
+    WrenType t = wrenGetSlotType(vm, 2);
+    CHECK(t == WREN_TYPE_LIST, "must pass a list to execv");
+    int argc = wrenGetListCount(vm, 2);
+    const char** env = malloc((argc + 1) * sizeof(char*));
+    env[argc] = NULL;
+    for (int i = 0; i < argc; ++i) {
+      wrenGetListElement(vm, 2, i, 3);
+      const char* warg = wrenGetSlotString(vm, 3);
+      env[i] = warg;
+    }
+    execve(path, argv, env);
+  } else {
+    execvp(path, argv);
+  }
+
+  CHECK(false, "execv failed");
+}
+
+
 WrenForeignMethodFn bindForeignMethod(
     WrenVM* vm,
     const char* module,
@@ -121,7 +194,9 @@ WrenForeignMethodFn bindForeignMethod(
   if (!strcmp(signature, "arg(_)")) return wrenArg;
   if (!strcmp(signature, "env(_)")) return wrenEnv;
   if (!strcmp(signature, "exit(_)")) return wrenExit;
-  fprintf(stderr, "unexpected foreign method");
+  if (!strcmp(signature, "execv(_)")) return wrenExec;
+  if (!strcmp(signature, "execv(_,_)")) return wrenExec;
+  fprintf(stderr, "unexpected foreign method %s", signature);
   exit(1);
 }
 
@@ -149,7 +224,9 @@ void usage() {
     "  io.write(s): write to stdout\n"
     "  io.arg(i): read args[i]\n"
     "  io.env(name): read env var\n"
-    "  io.exit(c): exit with code c\n";
+    "  io.exit(c): exit with code c\n"
+    "  io.execv(argv): replace current process with argv\n"
+    "  io.execv(argv, env): replace current process with argv and env\n";
   fputs(usage_str, stderr);
   exit(0);
 }
@@ -161,8 +238,18 @@ int main(int argc, char** argv) {
 
   Ctx ctx = {.argc = argc, .argv = argv};
   WrenVM* wren = setupWren(&ctx);
-  char* io_src = "class io {\n  foreign static write(s)\n  foreign static read()\n  foreign static read(n)\n  foreign static arg(i)\n  foreign static env(name)\n  foreign static exit(c)\n}";
-  CHECK(wrenInterpret(wren, "io", io_src) == WREN_RESULT_SUCCESS);
+  char* io_src = \
+      "class io {\n"
+      "  foreign static write(s)\n"
+      "  foreign static read()\n"
+      "  foreign static read(n)\n"
+      "  foreign static arg(i)\n"
+      "  foreign static env(name)\n"
+      "  foreign static exit(c)\n"
+      "  foreign static execv(argv)\n"
+      "  foreign static execv(argv, env)\n"
+      "}\n";
+  CHECK(wrenInterpret(wren, "io", io_src) == WREN_RESULT_SUCCESS, "bad io src");
   char* user_src = argv[argc - 1];
   CHECK(wrenInterpret(wren, "main", "import \"io\" for io") == WREN_RESULT_SUCCESS);
   CHECK(wrenInterpret(wren, "main", user_src) == WREN_RESULT_SUCCESS);
