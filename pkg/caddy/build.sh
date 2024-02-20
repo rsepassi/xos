6f9d12f58b1f0b4a267555630bb3538b5e46e772
Ryan Sepassi <rsepassi@gmail.com>
2024-02-19 16:15:28 -0800

    caddy added

---
+++ pkg/caddy/build.sh
@@ -0,0 +1,17 @@
+needtool golang
+PATH="$BUILD_TOOLDEPS/golang/bin:$PATH"
+
+url="https://github.com/caddyserver/caddy/archive/refs/tags/v2.7.6.tar.gz"
+hash="e1c524fc4f4bd2b0d39df51679d9d065bb811e381b7e4e51466ba39a0083e3ed"
+file="caddy.tar.gz"
+
+fetch "$url" "$file" "$hash"
+src=$(mktemp -d)
+untar "$BUILD_DEPS/$file" "$src"
+cd $src/cmd/caddy
+
+bin=$(zigi exe caddy)
+gobuild -o $bin
+
+mkdir "$BUILD_OUT/bin"
+mv $bin "$BUILD_OUT/bin"
