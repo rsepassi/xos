4b43d5fbf10c5e2a840722f55999a223e0ad2927
Ryan Sepassi <rsepassi@gmail.com>
2024-02-19 17:17:36 -0800

    wrensh windows fix

--- pkg/wrensh/build.sh
+++ pkg/wrensh/build.sh
@@ -1,5 +1,5 @@
 mkdir -p $BUILD_OUT/bin
-cc -O$OPT --target=$ARCH -s -static -o $BUILD_OUT/bin/wrensh \
+cc -O$OPT --target=$ARCH -s -static -o $BUILD_OUT/bin/$(zigi exe wrensh) \
   -I $BUILD_DEPS/wren/include \
   $BUILD_PKG/wrensh.c \
-  $BUILD_DEPS/wren/lib/libwren.a -lc
+  $BUILD_DEPS/wren/lib/$(zigi lib wren) -lc
