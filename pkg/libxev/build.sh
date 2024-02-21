867a400c85d9508e5c7adc1e36fd576d3c2f3650
Ryan Sepassi <rsepassi@gmail.com>
2024-02-20 18:04:37 -0800

    libxev removed

--- pkg/libxev/build.sh
+++
@@ -1,9 +0,0 @@
-file=libxev.tar.gz
-fetch_urltxt $BUILD_PKG/url.txt $file
-src=$(mktemp -d)
-untar $BUILD_DEPS/$file $src
-cd $src
-zig build -Dtarget=$ARCH -Doptimize=$OPT_ZIG
-
-ln -s $PWD/zig-out/include $BUILD_OUT
-ln -s $PWD/zig-out/lib $BUILD_OUT
