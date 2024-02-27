31eaa5f0d0c318197074877ca0066296f58c19da
Ryan Sepassi <rsepassi@gmail.com>
2024-02-27 11:17:19 -0800

    brotli fixed

--- pkg/brotli/build.sh
+++ pkg/brotli/build.sh
@@ -1,5 +1,5 @@
 fetch \
-  "https://api.github.com/repos/hexops/brotli/tarball/62ab0d6"
+  "https://api.github.com/repos/hexops/brotli/tarball/62ab0d6" \
   brotli.tar.gz \
   "b16f3ce6de16e3608048511f190179d316f52f72639654c8007ac0e65ad6ad4a"
 src=$(mktemp -d)
