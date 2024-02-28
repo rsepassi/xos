a9f0045d1606bf0b78f327f3d7c203a3bd5d7983
Ryan Sepassi <rsepassi@gmail.com>
2024-02-27 19:04:10 -0800

    mbedtls pc added

--- pkg/mbedtls/build.sh
+++ pkg/mbedtls/build.sh
@@ -7,5 +7,8 @@ cd $src
 cp $BUILD_PKG/build.zig .
 zig build -Dtarget=$TARGET -Doptimize=$OPT_ZIG
 
-ln -s $PWD/zig-out/lib $BUILD_OUT
+mv $PWD/zig-out/lib $BUILD_OUT
 find include -type f -name '*.h' -exec cp --parents {} $BUILD_OUT \;
+pkg-config --gendefault mbedtls
+pkg-config --gendefault mbedcrypto
+pkg-config --gendefault mbedx509
