ef023aec0565d58157bed4072042a1c7f2ba48f6
Ryan Sepassi <rsepassi@gmail.com>
2024-03-20 15:35:24 -0700

    wren meta added

--- pkg/wren/build.sh
+++ pkg/wren/build.sh
@@ -1,17 +1,23 @@
-fetch_urltxt $BUILD_PKG/url.txt wren.tar.gz
-src=$(mktemp -d)
-untar $BUILD_DEPS/wren.tar.gz $src
+url="https://github.com/wren-lang/wren/archive/refs/tags/0.4.0.tar.gz"
+hash="23c0ddeb6c67a4ed9285bded49f7c91714922c2e7bb88f42428386bf1cf7b339"
+src=$(fetch_untar $url wren.tar.gz $hash)
+cd $src
 
 # lib
-cd $src/src/vm
 touch wren.c
-zig build-lib -target $TARGET -O $OPT_ZIG wren.c *.c \
-  -I "$src"/src/include -DWREN_OPT_META=0 -DWREN_OPT_RANDOM=0 \
+zig build-lib -target $TARGET -O $OPT_ZIG \
+  wren.c \
+  src/vm/*.c \
+  src/optional/wren_opt_meta.c \
+  -DWREN_OPT_META=1 -DWREN_OPT_RANDOM=0 \
+  -I src/include \
+  -I src/optional \
+  -I src/vm \
   -lc
 
 # install
-out="$BUILD_OUT"
-mkdir -p $out/lib $out/include $out/pkgconfig
-cp "$src"/src/include/wren.h $out/include
-cp "$src"/src/vm/$(zigi lib wren) $out/lib
+cd "$BUILD_OUT"
+mkdir lib include
+cp "$src"/src/include/wren.h include
+cp "$src"/$(zigi lib wren) lib
 pkg-config --gendefault wren
