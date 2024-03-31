71da376045a6dc804058de70a6062c1d70aeea3b
Ryan Sepassi <rsepassi@gmail.com>
2024-03-31 15:33:07 -0700

    linuxsdk added, uses podman to extract include+lib, initially just from alpine

---
+++ pkg/linuxsdk/build.sh
@@ -0,0 +1,49 @@
+base=$1
+if [ "$base" = "alpine" ]
+then
+  base="alpine:3.19"
+else
+  >&2 echo "unsupported base $base"
+  exit 1
+fi
+
+pkgs=""
+oldifs=$IFS
+IFS=","
+for pkg in $2
+do
+  pkgs="$pkgs $pkg"
+done
+IFS=$oldifs
+
+libs=""
+oldifs=$IFS
+IFS=","
+for lib in $3
+do
+  libs="$libs $lib"
+done
+IFS=$oldifs
+
+export HOME=$XOS_SYSTEM_HOME
+
+arch=$TARGET_ARCH
+if [ "$TARGET_ARCH" = "aarch64" ]
+then
+  arch="arm64"
+fi
+
+id=$(system podman run --arch $arch -d alpine:3.19 sleep 10000000)
+system podman exec $id apk add $pkgs
+system podman export $id -o export.tar.gz
+system podman kill $id
+
+mkdir export
+tar xf export.tar.gz -C export
+mv export/usr/include "$BUILD_OUT"
+
+mkdir "$BUILD_OUT"/lib
+for lib in $libs
+do
+  cp export/usr/lib/lib$lib.so "$BUILD_OUT"/lib
+done
