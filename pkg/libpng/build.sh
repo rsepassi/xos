b1df8f65878d70ff271f7d3cafaf9fa1c5c5e488
Ryan Sepassi <rsepassi@gmail.com>
2024-04-13 14:53:03 -0700

    update libpng

--- pkg/libpng/build.sh
+++ pkg/libpng/build.sh
@@ -6,17 +6,35 @@ file="libpng.tar.gz"
 src=$(fetch_untar "$url" "$file" "$hash")
 cd $src
 
+if [ "$TARGET_ARCH" = "aarch64" ]
+then
+  extras="$(ls arm/*)"
+elif [ "$TARGET_ARCH" = "x86_64" ]
+then
+  extras="$(ls intel/*)"
+fi
+
 rm pngtest.c
+
 cp "$BUILD_PKG/config.h" .
 cp "$BUILD_PKG/pnglibconf.h" .
 zig build-lib -target $TARGET -O $OPT_ZIG \
   -DHAVE_CONFIG_H \
   -isystem . \
   $(pkg-config --cflags zlib/z) \
-  png.c png*.c \
+  png.c png*.c $extras \
+  -lc
+
+zig build-exe -target $TARGET -O $OPT_ZIG \
+  -I. \
+  "$BUILD_PKG/demo.c" \
+  $(zigi lib png) \
+  $(pkg-config --libs zlib/z) \
   -lc
 
 cd "$BUILD_OUT"
-mkdir lib include
+mkdir lib include bin
 cp "$src/png.h" include
 mv "$src/$(zigi lib png)" lib
+mv "$src/$(zigi exe demo)" bin
+pkg-config --gendefault png --deps zlib/z
