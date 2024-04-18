25b9a0288e5cf8e91f172bc71e9c22f19392fec6
Ryan Sepassi <rsepassi@gmail.com>
2024-04-18 16:00:47 -0700

    rm go binaries that can just use goinstall

--- pkg/goinstall/build.sh
+++ pkg/goinstall/build.sh
@@ -2,7 +2,14 @@ needtool golang
 
 PATH="$BUILD_TOOLS/golang/bin:$PATH"
 
+if [ "$TARGET_OS" = "$HOST_OS" ] && [ "$TARGET_ARCH" = "$HOST_ARCH" ]
+then
+  suffix=""
+else
+  suffix="$(goi os)_$(goi arch)"
+fi
+
 GOOS=$(goi os) GOARCH=$(goi arch) \
 go install -ldflags="-linkmode=external -extldflags -s -w" "$@"
 
-ln -s "$HOME/go/bin/$(goi os)_$(goi arch)" "$BUILD_OUT/bin"
+ln -s "$HOME/go/bin/$suffix" "$BUILD_OUT/bin"
