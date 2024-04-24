7bc1a139d7c89f747a6c0691396a677e4c25b26c
Ryan Sepassi <rsepassi@gmail.com>
2024-04-23 18:29:29 -0700

    add lmdb-zig bindings

---
+++ pkg/lmdb-zig/build.sh
@@ -0,0 +1,5 @@
+src=$(fetch_untar "https://api.github.com/repos/rsepassi/zig-lmdb/tarball/8bd7331" \
+  "lmdb-zig" \
+  "d9b9b51dd391fa0415daa75d0d579aff7d600cb6a0353676ecd757e5f90f6e19")
+
+ln -s "$src/src" "$BUILD_OUT/zig"
